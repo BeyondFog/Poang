5bd43f4555327214d36bcc42bfc77547bb59526e
Stephen Bronstein <steve@bronstein.org>
2012-05-10 13:26:19 -0700

    add before function to startup server for zombie tests

--- test/test_zombie.js
+++ test/test_zombie.js
@@ -1,13 +1,24 @@
 var assert = require('assert')
+  , app = require('../app')
   , index = require('../routes')
   , should = require('should')
   , zombie = require('zombie')
   ;
 
+var TEST_PORT = Math.floor(Math.random()*65535)
+var base_url = "http://localhost:" + TEST_PORT + "/";
+
+
+before(function() {
+   var server = app.init();
+   server.listen(TEST_PORT);
+   console.log("Server is listening on port %s", TEST_PORT);
+ });
+
 describe('front page', function() {
   it('should load', function (done) {
     var browser = new zombie();
-    browser.visit("http://localhost:3000/", function () {
+    browser.visit(base_url, function () {
       assert.ok(browser.success);
       if (browser.error) {
         console.dir("errors reported:", browser.errors);
@@ -18,7 +29,7 @@ describe('front page', function() {
   
   it('should have the page title Poang', function(done) {
     var browser = new zombie();
-    browser.visit("http://localhost:3000/", function () {
+    browser.visit(base_url, function () {
       assert.equal(browser.text("title"), "Poang");      
       if (browser.error) {
         console.dir("errors reported:", browser.errors);
@@ -29,7 +40,7 @@ describe('front page', function() {
   
   it('should successfully register', function (done) {
     var browser = new zombie();
-    browser.visit("http://localhost:3000/register", function () {
+    browser.visit(base_url + "register", function () {
       assert.ok(browser.query("#register"));
       // Fill email, password and submit form
       browser.
